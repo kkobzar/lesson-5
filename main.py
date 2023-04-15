d4133f346ac763ba06cd12b5c21ddd3be1c2399d
Kyryl Kobzar <kirilkobzar@gmail.com>
2023-04-15 11:41:20 +0300

    handling errors

--- main.py
+++ main.py
@@ -2,12 +2,6 @@ import requests
 import json
 from colorama import Fore, Back, Style
 
-
-r = requests.get('https://randomuser.me/api')
-
-person = json.loads(r.text)
-
-
 class Person:
     def __init__(self, name, surname, age, gender):
         self.name = name
@@ -16,17 +10,26 @@ class Person:
         self.isMen = gender == 'male'
 
 
-api_name = person['results'][0]['name']['first']
-api_surname = person['results'][0]['name']['last']
-api_age = person['results'][0]['dob']['age']
-api_gender = person['results'][0]['gender']
+try:
 
-random_person = Person(api_name, api_surname, api_age, api_gender)
+    r = requests.get('https://randomuser.me/api')
 
-if random_person.isMen:
-    print(Fore.BLUE + random_person.name, random_person.surname, random_person.age)
-else:
-    print(Fore.MAGENTA + random_person.name, random_person.surname, random_person.age)
+    person = json.loads(r.text)
+    api_name = person['results'][0]['name']['first']
+    api_surname = person['results'][0]['name']['last']
+    api_age = person['results'][0]['dob']['age']
+    api_gender = person['results'][0]['gender']
+
+    random_person = Person(api_name, api_surname, api_age, api_gender)
+
+    if random_person.isMen:
+        print(Fore.BLUE + random_person.name, random_person.surname, random_person.age)
+    else:
+        print(Fore.MAGENTA + random_person.name, random_person.surname, random_person.age)
+
+except Exception as e:
+    print(e)
+
 
 
 
