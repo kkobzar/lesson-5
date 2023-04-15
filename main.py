904142eb46281fb3e8f3163c3c64633d5297ffa0
Kyryl Kobzar <kirilkobzar@gmail.com>
2023-04-15 10:31:49 +0300

    got name/surname

--- main.py
+++ main.py
@@ -12,3 +12,8 @@ class Person:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
+
+api_name = person['results'][0]['name']['first']
+api_surname = person['results'][0]['name']['last']
+
+print(api_name, api_surname)
