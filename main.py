b2e42fb95f1a380a36a97e94f74b4789cdbbb2e2
Kyryl Kobzar <kirilkobzar@gmail.com>
2023-04-15 10:42:14 +0300

    added age

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ r = requests.get('https://randomuser.me/api')
 
 person = json.loads(r.text)
 
-print(person['results'][0]['email'])
-
 
 class Person:
-    def __init__(self, name, surname):
+    def __init__(self, name, surname, age):
         self.name = name
         self.surname = surname
+        self.age = age
 
 api_name = person['results'][0]['name']['first']
 api_surname = person['results'][0]['name']['last']
+api_age = person['results'][0]['dob']['age']
 
-print(api_name, api_surname)
+
+random_person = Person(api_name, api_surname, api_age)
+
+print(random_person.name, random_person.surname, api_age)
