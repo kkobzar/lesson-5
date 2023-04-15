f6cbf4722d6a18cc63acf714539a3ad9becfaca6
Kyryl Kobzar <kirilkobzar@gmail.com>
2023-04-15 11:26:28 +0300

    added person color

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
 import json
+from colorama import Fore, Back, Style
+
 
 r = requests.get('https://randomuser.me/api')
 
@@ -7,16 +9,30 @@ person = json.loads(r.text)
 
 
 class Person:
-    def __init__(self, name, surname, age):
+    def __init__(self, name, surname, age, gender):
         self.name = name
         self.surname = surname
         self.age = age
+        self.isMen = gender == 'male'
+
 
 api_name = person['results'][0]['name']['first']
 api_surname = person['results'][0]['name']['last']
 api_age = person['results'][0]['dob']['age']
+api_gender = person['results'][0]['gender']
+
+random_person = Person(api_name, api_surname, api_age, api_gender)
+
+if random_person.isMen:
+    print(Fore.BLUE + random_person.name, random_person.surname, random_person.age)
+else:
+    print(Fore.MAGENTA + random_person.name, random_person.surname, random_person.age)
 
 
-random_person = Person(api_name, api_surname, api_age)
 
-print(random_person.name, random_person.surname, api_age)
+# try:
+#     print('23' / 0)
+# except ZeroDivisionError:
+#     print('На нуль ділити не можна')
+# except TypeError:
+#     print('Щось не те з типами')
